98e8368f80509f97ce083eed677539d30da3fad2
Todd Hiestand <todd@343consulting.com>
2018-03-23 11:27:43 -0700

    Format the blog a bit

--- index.php
+++ index.php
@@ -7,9 +7,10 @@ get_header();
 <div class="wide-container content-container">
 	<div class="container row">		
 		<main role="main" class="row gutters">
-			<div class="col span_16">
+			<div class="col span_16 centered">
 				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 				<div class="article">
+						<?php the_post_thumbnail('image-wide'); ?>						
 						<h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
 						<?php the_excerpt(); ?>
 				</div>
@@ -18,7 +19,6 @@ get_header();
 				<?php endif; ?>
 				<?php get_template_part( 'inc/feature', 'pagination' ); ?>
 			</div>
-			<?php get_sidebar();?>
 		</main>
 </div>
 
