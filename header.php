48807e17b15827f2076a4533db8b7f644d3aec07
Todd Hiestand <todd@343consulting.com>
2018-03-22 23:44:43 -0700

    Fixing some of the basic layout stuff

--- header.php
+++ header.php
@@ -9,7 +9,6 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 	
 		<title><?php bloginfo( 'name' ); ?><?php wp_title( '|' ); ?></title>
-		<link rel="stylesheet" media="all" href="<?php bloginfo('template_directory'); ?>/css/responsive.gs.24col.css" />
 	
 	<!-- if page is content page -->
 	<?php if (is_single()) { ?>
