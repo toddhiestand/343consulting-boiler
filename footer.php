927c93c2cb88328f5769aabbdf5be5e2edf740b6
Todd Hiestand <todd@343consulting.com>
2018-03-22 23:02:59 -0700

    add scroll to elements

--- footer.php
+++ footer.php
@@ -41,6 +41,60 @@
 	});
 
 
+	
+		 // Scroll to Div for the home page
+
+function scrollToElement(selector, time, verticalOffset) {
+	time = typeof(time) != 'undefined' ? time : 500;
+	verticalOffset = typeof(verticalOffset) != 'undefined' ? verticalOffset : -60;
+	element = $(selector);
+	offset = element.offset();
+	offsetTop = offset.top + verticalOffset;
+	$('html, body').animate({
+		scrollTop: offsetTop
+	}, time);		
+}
+			
+$(document).ready(function() {
+		
+	$('#scroll-to-scrollsection1').click(function (e) {
+		e.preventDefault();
+		scrollToElement('#scrollsection1');
+	});
+
+	$('#scroll-to-scrollsection2').click(function (e) {
+			e.preventDefault();
+			scrollToElement('#scrollsection2');
+		});
+
+	$('#scroll-to-scrollsection3').click(function (e) {
+			e.preventDefault();
+			scrollToElement('#scrollsection3');
+		});
+
+	$('#scroll-to-scrollsection4').click(function (e) {
+			e.preventDefault();
+			scrollToElement('#scrollsection4');
+		});
+
+	$('#scroll-to-scrollsection5').click(function (e) {
+			e.preventDefault();
+			scrollToElement('#scrollsection5');
+		});
+
+	$('#scroll-to-scrollsection6').click(function (e) {
+			e.preventDefault();
+			scrollToElement('#scrollsection6');
+		});
+
+	$('#scroll-to-scrollsection7').click(function (e) {
+			e.preventDefault();
+			scrollToElement('#scrollsection7');
+		});
+
+	
+});
+
 </script>
 
 <?php wp_footer();?>
