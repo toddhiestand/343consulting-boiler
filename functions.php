8ec9dcd6c8624b37489cc06b0f8fbabdc05cc6f7
Todd Hiestand <todd@343consulting.com>
2018-03-22 22:26:30 -0700

    edits to functions file

--- functions.php
+++ functions.php
@@ -55,14 +55,11 @@ add_action( 'init', 'register_my_menus' );
 function enqueue_scripts() {
 
   wp_register_style( 'style', get_template_directory_uri() . '/style.css', array(), '', 'all' );
+  wp_register_style( 'grid', get_template_directory_uri() . '/css/responsive.gs.24col.css', array(), '', 'all' );
   wp_register_style( 'ss-air', get_template_directory_uri() . '/fonts/ss-air/ss-air.css', array(), '', 'all' );
   // wp_register_style( 'ss-pika', get_template_directory_uri() . '/fonts/symbolset/ss-pika.css', array(), '', 'all' );
   wp_register_style( 'ss-social', get_template_directory_uri() . '/fonts/ss-social-circle/ss-social-circle.css', array(), '', 'all' );
   wp_register_style( 'bxcss', get_template_directory_uri() . '/css/jquery.bxslider.css', array(), '', 'all' );
-  wp_register_style( 'grid', get_template_directory_uri() . '/css/responsive.gs.24col.css', array(), '', 'all' );
-
-  
-  wp_register_style( 'responsive', get_template_directory_uri() . '/css/responsive.gs.24col.css', array(), '', 'all' );
   wp_register_style( 'pushy', get_template_directory_uri() . '/css/pushy.css', array(), '', 'all' );
   wp_register_style( 'mobile', get_template_directory_uri() . '/css/mobile.css', array(), '', 'all' );
   wp_register_style( 'menu', get_template_directory_uri() . '/css/menu.css', array(), '', 'all' );
@@ -80,12 +77,9 @@ function enqueue_scripts() {
   // wp_enqueue_style('ss-pika');
   wp_enqueue_style('ss-air');
   wp_enqueue_style('bxcss');
-  wp_enqueue_style('responsive');
   wp_enqueue_style('pushy');
   wp_enqueue_style('menu');
   wp_enqueue_style('mobile');
-  
-
 
   wp_enqueue_script( 'jquery' );
   wp_enqueue_script( 'modernizer' );
@@ -134,62 +128,10 @@ add_filter('excerpt_more', 'new_excerpt_more');
  add_action( 'pre_ping', 'no_self_ping' );
 
 
-
-/**
- * Add additional custom field
- */
-
-add_action ( 'show_user_profile', 'my_show_extra_profile_fields' );
-add_action ( 'edit_user_profile', 'my_show_extra_profile_fields' );
-
-function my_show_extra_profile_fields ( $user )
-{
-?>
-    <h3>Extra profile information</h3>
-    <table class="form-table">
-        <tr>
-            <th><label for="twitter">Twitter</label></th>
-            <td>
-                <input type="text" name="twitter" id="twitter" value="<?php echo esc_attr( get_the_author_meta( 'twitter', $user->ID ) ); ?>" class="regular-text" /><br />
-                <span class="description">Please enter your Twitter username.</span>
-            </td>
-        </tr>
-        <tr>
-            <th><label for="facebook">Facebook</label></th>
-            <td>
-                <input type="text" name="facebook" id="twitter" value="<?php echo esc_attr( get_the_author_meta( 'facebook', $user->ID ) ); ?>" class="regular-text" /><br />
-                <span class="description">Please add the link to your Facebook page.</span>
-            </td>
-        </tr>
-        <tr>
-            <th><label for="instagram">Instagram</label></th>
-            <td>
-                <input type="text" name="instagram" id="instagram" value="<?php echo esc_attr( get_the_author_meta( 'instagram', $user->ID ) ); ?>" class="regular-text" /><br />
-                <span class="description">Please add the link to your Instagram page.</span>
-            </td>
-        </tr>
-    </table>
-<?php
-}
-
-add_action ( 'personal_options_update', 'my_save_extra_profile_fields' );
-add_action ( 'edit_user_profile_update', 'my_save_extra_profile_fields' );
-
-function my_save_extra_profile_fields( $user_id )
-{
-    if ( !current_user_can( 'edit_user', $user_id ) )
-        return false;
-    /* Copy and paste this line for additional fields. Make sure to change 'twitter' to the field ID. */
-    update_user_meta( $user_id, 'twitter', $_POST['twitter'] );
-    update_user_meta( $user_id, 'facebook', $_POST['facebook'] );
-    update_user_meta( $user_id, 'instagram', $_POST['instagram'] );
-}
-
-
 // Add custom branding to the footer of the admin
  
 function modify_footer_admin () {
-  echo 'Created by <a href="http://www.343design.net">343design</a>.';
+  echo 'Created by <a href="http://www.343consulting.com">343 Consulting</a>.';
 }
 
 add_filter('admin_footer_text', 'modify_footer_admin');
